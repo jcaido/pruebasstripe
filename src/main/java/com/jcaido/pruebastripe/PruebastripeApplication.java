9cb6f373f8a1fd50a78c9992ecbd0dd1c5e333a4
Cristobal Rosa <crisrogar@gmail.com>
2024-07-22 11:42:24 +0200

    first commit

---
+++ src/main/java/com/jcaido/pruebastripe/PruebastripeApplication.java
@@ -0,0 +1,13 @@
+package com.jcaido.pruebastripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebastripeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebastripeApplication.class, args);
+	}
+
+}
