657de0072b1077a523f9a3efc2f1e76110b38e8b
Cristobal Rosa <crisrogar@gmail.com>
2024-07-22 12:44:00 +0200

    create payload message

---
+++ src/main/java/com/jcaido/pruebastripe/payload/message.java
@@ -0,0 +1,14 @@
+package com.jcaido.pruebastripe.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class message {
+    private String message;
+}
