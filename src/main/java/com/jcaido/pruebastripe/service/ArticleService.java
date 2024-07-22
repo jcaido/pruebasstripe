b6d23d0c8683837af4c98ee783213d4da28f43f8
Cristobal Rosa <crisrogar@gmail.com>
2024-07-22 12:34:38 +0200

    create method gelAll in ArticleService

--- src/main/java/com/jcaido/pruebastripe/service/ArticleService.java
+++ src/main/java/com/jcaido/pruebastripe/service/ArticleService.java
@@ -5,9 +5,15 @@ import com.jcaido.pruebastripe.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ArticleService {
     @Autowired
     ArticleRepository articleRepository;
+
+    public List<Article> getAll() {
+        return articleRepository.findAll();
+    }
 
 }
