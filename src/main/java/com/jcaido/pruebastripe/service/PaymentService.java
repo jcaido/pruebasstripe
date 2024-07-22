5798f4d80e431fc329b00d920f2ff1c81048b41e
Cristobal Rosa <crisrogar@gmail.com>
2024-07-22 19:37:09 +0200

    create method paymentIntent in PaymentService

--- src/main/java/com/jcaido/pruebastripe/service/PaymentService.java
+++ src/main/java/com/jcaido/pruebastripe/service/PaymentService.java
@@ -2,7 +2,9 @@ package com.jcaido.pruebastripe.service;
 
 import com.jcaido.pruebastripe.payload.PaymentIntentDTO;
 import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,19 @@ public class PaymentService {
     @Value("${stripe.key.secret}")
     String secretKey;
 
-    public PaymentIntent paymentIntent(PaymentIntentDTO paymentIntentDTO) {
+    public PaymentIntent paymentIntent(PaymentIntentDTO paymentIntentDTO) throws StripeException {
         Stripe.apiKey = secretKey;
 
+        PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
+                .setAmount(paymentIntentDTO.getAmount())
+                .setCurrency(paymentIntentDTO.getCurrency().toString())
+                .setAutomaticPaymentMethods(
+                        PaymentIntentCreateParams.AutomaticPaymentMethods.builder()
+                                .setEnabled(true)
+                                .build()
+                )
+                .build();
+
+        return PaymentIntent.create(params);
     }
 }
