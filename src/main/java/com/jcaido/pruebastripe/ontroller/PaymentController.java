1f8e1412f7a76d1e99c9eefdd7ad1906cca24027
Cristobal Rosa <crisrogar@gmail.com>
2024-07-23 11:15:56 +0200

    create method payment in PaymentController

---
+++ src/main/java/com/jcaido/pruebastripe/ontroller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.jcaido.pruebastripe.ontroller;
+
+import com.jcaido.pruebastripe.payload.PaymentIntentDTO;
+import com.jcaido.pruebastripe.service.PaymentService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/stripe")
+@CrossOrigin(origins = "*")
+public class PaymentController {
+
+    @Autowired
+    PaymentService paymentService;
+
+    @PostMapping("/paymentintent")
+    public ResponseEntity<String> payment(@RequestBody PaymentIntentDTO paymentIntentDTO) throws StripeException {
+        PaymentIntent paymentIntent = paymentService.paymentIntent(paymentIntentDTO);
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<String>(paymentStr, HttpStatus.OK);
+    }
+}
