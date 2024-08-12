e35937f80b0f7ec054a4ec5fd5b09393b75ecb21
Cristobal Rosa <crisrogar@gmail.com>
2024-08-12 11:44:52 +0200

    create method generateReceiptPdf in Payment service

--- src/main/java/com/jcaido/pruebastripe/service/PaymentService.java
+++ src/main/java/com/jcaido/pruebastripe/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.jcaido.pruebastripe.service;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.jcaido.pruebastripe.payload.PaymentIntentDTO;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
@@ -9,6 +11,10 @@ import com.stripe.param.PaymentIntentConfirmParams;
 import com.stripe.param.PaymentIntentCreateParams;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @Service
 public class PaymentService {
@@ -52,5 +58,27 @@ public class PaymentService {
         PaymentIntentCancelParams params = PaymentIntentCancelParams.builder().build();
 
         return resource.cancel(params);
+    }
+
+    public ByteArrayInputStream generateReceiptPdf(PaymentIntent paymentIntent) throws DocumentException, IOException {
+        Document document = new Document();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        PdfWriter.getInstance(document, out);
+        document.open();
+
+        Font fontTitle = new Font(Font.FontFamily.TIMES_ROMAN, 20, Font.BOLD);
+        Paragraph title = new Paragraph("recibo", fontTitle);
+        title.setAlignment(Element.ALIGN_CENTER);
+        document.add(title);
+
+        document.add(new Paragraph("Payment Intent ID: " + paymentIntent.getId()));
+        document.add(new Paragraph("Amount: " + paymentIntent.getAmount() / 100 + " EUR"));
+        document.add(new Paragraph("Currency: " + paymentIntent.getCurrency().toUpperCase()));
+        document.add((new Paragraph("Status: " + paymentIntent.getStatus())));
+
+        document.close();
+
+        return new ByteArrayInputStream(out.toByteArray());
     }
 }
